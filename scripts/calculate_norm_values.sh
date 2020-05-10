903f5ec309090e672098ece60b6e7ef706196c05
Seungwook Han <sh3264@columbia.edu>
2020-05-10 19:30:48 -0400

    Chmod add exec

--- scripts/calculate_norm_values.sh
+++ scripts/calculate_norm_values.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python calculate_norm_values.py \
---dataset WT64 --data_root /data/ImageNet/ILSVRC2012/train/
+--dataset WT64 --data_root /data/ImageNet/ILSVRC2012/train/ \
+--num_workers=2
