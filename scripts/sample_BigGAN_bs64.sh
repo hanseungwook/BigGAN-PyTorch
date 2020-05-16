f318b7d420ad14a88e45837370da4ebe5d438f9f
Seungwook Han <sh3264@coumbia.edu>
2020-05-15 20:15:46 -0400

    Added load weights'

--- scripts/sample_BigGAN_bs64.sh
+++ scripts/sample_BigGAN_bs64.sh
@@ -20,4 +20,5 @@ python sample.py \
 --test_every 2000 --save_every 1000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
 --G_batch_size 512 --sample_trunc_curves 0.05_0.05_1.0  \
 --sample_inception_metrics --sample_npz  --sample_random --sample_sheets --sample_interps \
---sample_num_npz 20000
+--sample_num_npz 20000 \
+--load_weights copy1
