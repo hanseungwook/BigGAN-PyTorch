Fixed normalization to [-1,1] and updated printing fid for iwt'ed to full

utils.py
@@ -410,7 +410,8 @@ dset_dict = {'I32': dset.ImageFolder, 'I64': dset.ImageFolder,
              'I128': dset.ImageFolder, 'I256': dset.ImageFolder,
              'I32_hdf5': dset.ILSVRC_HDF5, 'I64_hdf5': dset.ILSVRC_HDF5, 
              'I128_hdf5': dset.ILSVRC_HDF5, 'I256_hdf5': dset.ILSVRC_HDF5,
-             'WT64': dset.ImageFolder, 'WT64_hdf5': dset.ILSVRC_HDF5,
+             'WT64': dset.ImageFolder, 'WT64_hdf5': dset.ImageFolder,
+             'D128': dset.ImageFolder, 'D128_hdf5': dset.ImageFolder,
              'C10': dset.CIFAR10, 'C100': dset.CIFAR100}
 imsize_dict = {'I32': 32, 'I32_hdf5': 32,
                'I64': 64, 'I64_hdf5': 64,
@@ -504,10 +505,10 @@ def iwt(vres, inv_filters, levels=1):
   return res.reshape(bs, -1, h, w)
   
 def denormalize(x, shift, scale):
-  return x*scale - shift
+  return ((x * 0.5) + 0.5) * scale - shift
     
 def normalize(x, shift, scale):
-  return (x + shift) / scale
+  return (((x + shift) / scale) - 0.5) / 0.5
   
 def load_norm_dict(path):
   loaded = np.load(path)