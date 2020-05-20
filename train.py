2bbfbf7091d61e0a6005430504ad21446fa60f72
Seungwook Han <sh3264@coumbia.edu>
2020-05-20 19:25:59 -0400

    Removed contig

--- train.py
+++ train.py
@@ -187,9 +187,9 @@ def run(config):
       if config['ema']:
         G_ema.train()
       if config['D_fp16']:
-        x, y = x.contiguous().to(device).half(), y.contiguous().to(device)
+        x, y = x.to(device).half(), y.to(device)
       else:
-        x, y = x.contiguous().to(device), y.contiguous().to(device)
+        x, y = x.to(device), y.to(device)
       
       metrics = train(x, y)
       end_time = time.time()
