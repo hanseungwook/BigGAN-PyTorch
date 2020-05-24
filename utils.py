2d792b3f919aded411358b9c5ceaea74db82e0c8
Seungwook Han <sh3264@coumbia.edu>
2020-05-24 00:49:38 -0400

    Fixing data root dir

--- utils.py
+++ utils.py
@@ -654,7 +654,7 @@ def get_data_loaders(dataset, data_root=None, augment=False, batch_size=64,
                      **kwargs):
 
   # Append /FILENAME.hdf5 to root if using hdf5
-  data_root += '/%s' % root_dict[dataset]
+  # data_root += '/%s' % root_dict[dataset]
   # data_root = 
   print('Using dataset root location %s' % data_root)
 
