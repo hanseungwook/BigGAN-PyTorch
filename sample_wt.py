Fixed normalization for batch of tensors

sample_wt.py
@@ -115,14 +115,11 @@ def run(config):
         images_padded = utils.zero_pad(images, 256, 'cuda:1')
 
         images_iwt = utils.iwt(images_padded, inv_filters, levels=2)
+        
+        # Resize to 299 x 299 and normalize with respective mean & std for Inception V3
         images_iwt = F.interpolate(images_iwt, 299)
+        images_iwt = utils.normalize_batch(images_iwt, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-        preprocess = transforms.Compose([
-          transforms.ToTensor(),
-          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
-
-        images_iwt = preprocess(images_iwt)
         outputs = rejection_model(images_iwt)
         outputs = torch.nn.functional.softmax(outputs, dim=1)
 

utils.py
@@ -530,7 +530,15 @@ def denormalize_wt(x, shift, scale):
     
 def normalize_wt(x, shift, scale):
   return (((x + shift) / scale) - 0.5) / 0.5
-  
+
+def normalize_batch(x, mean, std):
+  y = x.new(*x.size())
+  y[:, 0, :, :] = x[:, 0, :, :] * std[0] + mean[0]
+  y[:, 1, :, :] = x[:, 1, :, :] * std[1] + mean[1]
+  y[:, 2, :, :] = x[:, 2, :, :] * std[2] + mean[2]
+
+  return y
+
 def load_norm_dict(path):
   loaded = np.load(path)
   