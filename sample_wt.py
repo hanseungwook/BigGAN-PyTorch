017583b3f6d2791aa20d99de1d3bbd76184dfe38
Seungwook Han <sh3264@coumbia.edu>
2020-06-29 17:19:29 +0900

    Fixing pil and tensor compat

--- sample_wt.py
+++ sample_wt.py
@@ -117,8 +117,10 @@ def run(config):
         images_iwt = utils.iwt(images_padded, inv_filters, levels=2)
 
         preprocess = transforms.Compose([
+          transforms.ToPILImage(),
           transforms.Resize(299),
           transforms.CenterCrop(299),
+          transforms.ToTensor(),
           transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
